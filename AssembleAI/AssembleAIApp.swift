20b34bc3f5545e25f5af27b914ed51619171f717
Mukul Anand <mukulanand.biz@gmail.com>
2026-08-24 14:47:09 +0530

    Initial Commit

---
+++ AssembleAI/AssembleAIApp.swift
@@ -0,0 +1,32 @@
+//
+//  AssembleAIApp.swift
+//  AssembleAI
+//
+//  Created by Mukul on 24/08/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AssembleAIApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ AssembleAITests/AssembleAITests.swift
@@ -0,0 +1,19 @@
+//
+//  AssembleAITests.swift
+//  AssembleAITests
+//
+//  Created by Mukul on 24/08/26.
+//
+
+import Testing
+@testable import AssembleAI
+
+struct AssembleAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
